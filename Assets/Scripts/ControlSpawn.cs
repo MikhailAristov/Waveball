5dfcf3645df02e79d9392cace8b2e6cc6544d4e1
Mikhail Aristov <mikhail.aristov@googlemail.com>
2017-01-20 22:23:31 +0100

    Zwischestand particle spawn

---
+++ Assets/Scripts/ControlSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject prefabParticle = Resources.Load("Particle") as GameObject;
+		Vector3 spawnPos = transform.position + transform.up * 0.6f;
+		GameObject particle = Instantiate(prefabParticle, spawnPos, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
