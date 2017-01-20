f250d3fef15e08299612ed617d81fdf1bc90b67f
Mikhail Aristov <mikhail.aristov@googlemail.com>
2017-01-20 22:52:09 +0100

    first milestone reached!

---
+++ Assets/Scripts/ControlParticle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlParticle : MonoBehaviour {
+
+	// TODO: Objecte poolen
+
+	private Rigidbody myRigidBody;
+
+	public float minimalVelocityBeforeDeath = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(myRigidBody.velocity.magnitude < minimalVelocityBeforeDeath) {
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.CompareTag("Finish")) {
+			Debug.LogError("YOU HAVE WON");
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Scripts/ControlSpawn.cs
+++ Assets/Scripts/ControlSpawn.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class ControlSpawn : MonoBehaviour {
 
+	public bool spawnActive = true;
+
 	// Use this for initialization
 	void Start () {
-		GameObject prefabParticle = Resources.Load("Particle") as GameObject;
-		Vector3 spawnPos = transform.position + transform.up * 0.6f;
-		GameObject particle = Instantiate(prefabParticle, spawnPos, Quaternion.identity);
+		StartCoroutine(keepSpawningParticles());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	private IEnumerator keepSpawningParticles() {
+		while(spawnActive) {
+			GameObject prefabParticle = Resources.Load("Particle") as GameObject;
+			Vector3 spawnPos = transform.position + transform.up * 0.7f;
+			GameObject particle = Instantiate(prefabParticle, spawnPos, Quaternion.identity);
+			particle.GetComponent<Rigidbody>().velocity = transform.up * 5.0f;
+			// Then wait
+			yield return new WaitForSeconds(2.0f);
+		}
 	}
 }
