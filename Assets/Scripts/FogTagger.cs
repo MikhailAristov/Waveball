36494d180721d611cc687274159a0129d1614b07
Christoph Michel <c.michel.ahorn@gmail.com>
2017-01-21 20:32:55 +0100

    meta added

---
+++ Assets/Scripts/FogTagger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogTagger: MonoBehaviour {
+
+	float[,] panels;
+
+
+	void Start() {
+		//panels = GetComponent<ControlSurface>().needlePool;
+
+		foreach (var panel in panels) {
+			//panel.tag 
+		}
+	}
+
+
+	void Update() {
+
+	}
+}
