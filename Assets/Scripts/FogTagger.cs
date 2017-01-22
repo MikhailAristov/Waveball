16fbaaa4588d570336c26e8dfb8748a98f9769bc
Christoph Michel <c.michel.ahorn@gmail.com>
2017-01-22 10:14:37 +0100

    fog tagger range changed

--- Assets/Scripts/FogTagger.cs
+++ Assets/Scripts/FogTagger.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 public class FogTagger: MonoBehaviour {
 
+	public const float IN_SIGHT_RANGE = 2.5f;
+
 	public ControlSurface controlSurface;
 	public GameObject[,] panels;
 
@@ -20,57 +22,27 @@ public class FogTagger: MonoBehaviour {
 
 	void Update() {
 		panels = controlSurface.gridPanels;
-
-//		Debug.Log (controlSpawn.particlePool.Count);
-//		for (int i = 0; i < controlSpawn.particlePool.Count; i++) {
-//			Debug.Log (controlSpawn.particlePool [i]);
-//		}
-//		foreach (var particle in controlSpawn.particlePool) {
-//			Debug.Log(particle);
-//		}
-
 		GameObject particle = controlSpawn.particle;
-		Debug.Log (particle);
 		Vector3 particleGridCoord = controlSurface.worldPosToGrid (particle.transform.position.x, particle.transform.position.z);
 
 		// update previous visibility
-		for (int x = 0; x < panels.GetLength(0); x++) {
-			for (int z = 0; z < panels.GetLength(1); z++) {
+		for (int x = 0; x < panels.GetLength (0); x++) {
+			for (int z = 0; z < panels.GetLength (1); z++) {
 				GameObject panel = panels [x, z];
 				if (panel.GetComponentInParent<ControlPanel> ().State == PanelFogState.InSight) {
 					panel.GetComponentInParent<ControlPanel> ().State = PanelFogState.Discovered;
 				}
-//				Debug.Log (particleGridCoord);
-//				Debug.Log (new Vector3(x, 0, z));
-//				Debug.Log ("-------");
 
-
-				if (isInRange(particleGridCoord, new Vector3(x, 0, z))) {
+				if (isInRange (particleGridCoord, new Vector3 (x, 0, z))) {
 					panel.GetComponentInParent<ControlPanel> ().State = PanelFogState.InSight;
 				}
 			}
 		}
-	
-		/*
-		GameObject particle = controlSpawn.particle;
-		Vector3 gridCoord = controlSurface.worldPosToGrid (particle.transform.position.x, particle.transform.position.z);
-
-		int range = 2;
-		Debug.Log("Particle");
-		for (int x = (int) gridCoord.x - range; x < (int) gridCoord.x + range; x++) {
-			for (int z = (int) gridCoord.z - range; z < (int) gridCoord.z + range; z++) {
-				if (x < 0 || x > panels.GetLength (0) || z < 0 || z > panels.GetLength (1)) {
-					continue;
-				}
-				panels [x, z].tag = "InSight";
-			}
-		}
-		*/
 	}
 	
-
 	bool isInRange(Vector3 a, Vector3 b) {
-		return Mathf.Abs (a.x - b.x) < 3 && Mathf.Abs (a.z - b.z) < 3;
+		//return Mathf.Abs (a.x - b.x) < 3 && Mathf.Abs (a.z - b.z) < 3;
+		return (a - b).magnitude < IN_SIGHT_RANGE;
 	}
 
 }
