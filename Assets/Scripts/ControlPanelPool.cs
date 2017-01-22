3d26896883bdf010d841733bc12026560ddb9fdd
Christoph Michel <c.michel.ahorn@gmail.com>
2017-01-22 10:46:04 +0100

    panel count added

---
+++ Assets/Scripts/ControlPanelPool.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanelPool : MonoBehaviour {
+
+
+	public float DiscoveryPercentage { get; set;}
+
+	// Use this for initialization
+	void Start () {		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var panels = gameObject.GetComponentsInChildren<ControlPanel> ();
+
+
+		int sumDiscovered = 0;
+		foreach (var panel in panels) {
+			if (panel.State == PanelFogState.Discovered || panel.State == PanelFogState.InSight) {
+				sumDiscovered++;
+			}
+		}
+
+		DiscoveryPercentage = (float) sumDiscovered / panels.Length;
+		Debug.Log (DiscoveryPercentage);
+	}
+}
