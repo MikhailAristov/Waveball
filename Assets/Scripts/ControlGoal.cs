24c13f43e33b6a02aeadfe96a757995594951e05
Mikhail Aristov <mikhail.aristov@googlemail.com>
2017-01-22 14:22:17 +0100

    Level transition on goal hit

---
+++ Assets/Scripts/ControlGoal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlGoal : MonoBehaviour {
+
+	public string nextLevelName;
+	public int hitCounter;
+	public int targetHitCount = 1;
+
+	// Use this for initialization
+	void Start () {
+		hitCounter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(hitCounter >= targetHitCount) {
+			hitCounter = 0;
+			SceneManager.LoadScene( nextLevelName, LoadSceneMode.Single );
+			SceneManager.LoadScene ( "IngameMenu", LoadSceneMode.Additive );
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if ( other.CompareTag ( "Player" ) )
+		{
+			hitCounter += 1;
+		}
+	}
+}
